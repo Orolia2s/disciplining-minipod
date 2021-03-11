d794ee61282de4f75c7da9f6ef262556fd9ba9bd
Charles Parent <charles.parent@orolia.com>
2021-03-11 19:02:56 +0100

    Use double arrays to store ctrl_load_nodes and ctrl_drift_coeffs
    	- Add ctrl_nodes_length containing the length of these 2 arrays
    	- Changes have been made inside config file to support this
    change

--- src/parameters.h
+++ src/parameters.h
@@ -7,12 +7,9 @@ struct parameters {
 	int ref_fluctuations_ns;
 	int phase_jump_threshold_ns;
 	int phase_resolution_ns;
-	double ctrl_load_node_0;
-	double ctrl_load_node_1;
-	double ctrl_load_node_2;
-	double ctrl_drift_coeffs_0;
-	double ctrl_drift_coeffs_1;
-	double ctrl_drift_coeffs_2;
+	int ctrl_nodes_length;
+	double *ctrl_load_nodes;
+	double *ctrl_drift_coeffs;
 };
 
 enum value_type {
@@ -20,6 +17,7 @@ enum value_type {
 	VALUE_TYPE_INT,
 	VALUE_TYPE_BOOL,
 	VALUE_TYPE_CHAR,
+	VALUE_TYPE_DOUBLE_ARRAY,
 };
 
 #define CONFIG_ENTRY(key, t) { \
@@ -40,24 +38,22 @@ static const struct config_key config_keys[] = {
 	CONFIG_ENTRY(ref_fluctuations_ns, INT),
 	CONFIG_ENTRY(phase_jump_threshold_ns, INT),
 	CONFIG_ENTRY(phase_resolution_ns, INT),
-	CONFIG_ENTRY(ctrl_load_node_0, DOUBLE),
-	CONFIG_ENTRY(ctrl_load_node_1, DOUBLE),
-	CONFIG_ENTRY(ctrl_load_node_2, DOUBLE),
-	CONFIG_ENTRY(ctrl_drift_coeffs_0, DOUBLE),
-	CONFIG_ENTRY(ctrl_drift_coeffs_1, DOUBLE),
-	CONFIG_ENTRY(ctrl_drift_coeffs_2, DOUBLE),
+	CONFIG_ENTRY(ctrl_nodes_length, INT),
+	CONFIG_ENTRY(ctrl_load_nodes, DOUBLE_ARRAY),
+	CONFIG_ENTRY(ctrl_drift_coeffs, DOUBLE_ARRAY),
 };
 
 static inline void print_parameters(struct parameters *params) {
 	printf("ref_fluctuations_ns: %d\n", params->ref_fluctuations_ns);
 	printf("phase_jump_threshold_ns: %d\n", params->phase_jump_threshold_ns);
 	printf("phase_resolution_ns: %d\n", params->phase_resolution_ns);
-	printf("ctrl_load_node_0: %f\n", params->ctrl_load_node_0);
-	printf("ctrl_load_node_1: %f\n", params->ctrl_load_node_1);
-	printf("ctrl_load_node_2: %f\n", params->ctrl_load_node_2);
-	printf("ctrl_drift_coeffs_0: %f\n", params->ctrl_drift_coeffs_0);
-	printf("ctrl_drift_coeffs_1: %f\n", params->ctrl_drift_coeffs_1);
-	printf("ctrl_drift_coeffs_2: %f\n", params->ctrl_drift_coeffs_2);
+	printf("ctrl_nodes_length: %d\n", params->ctrl_nodes_length);
+	for (int i = 0; i < params->ctrl_nodes_length; i++) {
+		printf("ctrl_load_nodes[%d]: is %f\n", i, params->ctrl_load_nodes[i]);
+	}
+	for (int i = 0; i < params->ctrl_nodes_length; i++) {
+		printf("ctrl_drift_coeffs[%d]: is %f\n", i, params->ctrl_drift_coeffs[i]);
+	}
 }
 
 int fill_parameters(struct parameters *p, const char *path, char err_msg[]);
