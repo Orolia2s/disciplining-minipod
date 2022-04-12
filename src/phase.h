dfbfddab3be8f46ca7f1c5bc37917d2cda268369
Charles Parent <charles.parent@orolia2s.com>
2022-04-12 11:55:11 +0200

    Clean unsused kalman structure

--- src/phase.h
+++ src/phase.h
@@ -6,7 +6,6 @@
 #include "algorithm_structs.h"
 #include "utils.h"
 
-float filter_phase(struct kalman_parameters *kalman, float phase, float estimated_drift, int interval);
 int compute_phase_error_mean(struct algorithm_input *inputs, int length, float *mean_phase_error);
 int compute_frequency_error(struct algorithm_input *inputs, int length, struct linear_func_param *func);
 
