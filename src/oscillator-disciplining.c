d6a8e6f46d99adf66ea135bb98135e9ce77e6eb6
Matthias Lorentz <matthiaslrtz@gmail.com>
2022-04-12 11:02:48 +0200

    condition for pcorr lowered to 0.666666*config->ref_fluctuations_ns

--- src/oscillator-disciplining.c
+++ src/oscillator-disciplining.c
@@ -760,7 +760,7 @@ int od_process(struct od *od, const struct od_input *input,
 					/* Compensate phase error */
 					float frequency_error_pcorr = 0.0;
 					int16_t delta_fine_pcorr = 0;
-					if (fabs(mean_phase_error) >= config->ref_fluctuations_ns) {
+					if (fabs(mean_phase_error) >= 0.6*config->ref_fluctuations_ns) {
 						frequency_error_pcorr = - mean_phase_error / (LOCK_LOW_RESOLUTION_PHASE_CONVERGENCE_REACTIVITY);
 						if (fabs(frequency_error_pcorr) > fabs((MRO_FINE_STEP_SENSITIVITY * 1.E9)))
 							delta_fine_pcorr = round(frequency_error_pcorr / (MRO_FINE_STEP_SENSITIVITY * 1.E9)); // check sign !!
@@ -946,7 +946,7 @@ int od_process(struct od *od, const struct od_input *input,
 					/* Compensate phase error */
 					float frequency_error_pcorr = 0.0;
 					int16_t delta_fine_pcorr = 0;
-					if (fabs(mean_phase_error) >= config->ref_fluctuations_ns) {
+					if (fabs(mean_phase_error) >= 0.6*config->ref_fluctuations_ns) {
 						frequency_error_pcorr = - mean_phase_error / (LOCK_HIGH_RESOLUTION_PHASE_CONVERGENCE_REACTIVITY);
 						if (fabs(frequency_error_pcorr) > fabs((MRO_FINE_STEP_SENSITIVITY * 1.E9)))
 							delta_fine_pcorr = round(frequency_error_pcorr / (MRO_FINE_STEP_SENSITIVITY * 1.E9));
