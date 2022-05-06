ad494fd02edbc326b0d3f4a2b97c46bf88114107
Charles Parent <charles.parent@orolia2s.com>
2022-05-06 11:07:37 +0200

    Update comments of phase files

--- src/phase.c
+++ src/phase.c
@@ -1,9 +1,44 @@
+/*
+ * liboscillator-disciplining: Disciplining Algorithm for Orolia's mRO50.
+ * Copyright (C) 2021  Spectracom SAS
+
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * @file phase.c
+ * @brief Functions to compute mean phase error and frequency error
+ * @date 2022-05-06
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #include <math.h>
 
 #include "phase.h"
 
 #include "log.h"
 
+/**
+ * @brief Compute mean phase error
+ *
+ * @param inputs array of input data containing gnss valid flag
+ * @param length array length
+ * @param mean_phase_error pointer where mean phase error will be stored
+ * @return int 0 in success else -1
+ */
 int compute_phase_error_mean(struct algorithm_input *inputs, int length, float *mean_phase_error)
 {
 	float sum_phase_error = 0.0;
@@ -22,6 +57,14 @@ int compute_phase_error_mean(struct algorithm_input *inputs, int length, float *
 	return 0;
 }
 
+/**
+ * @brief Compute frequency error
+ *
+ * @param inputs array of input data containing gnss valid flag
+ * @param length array length
+ * @param func parameters of the linear function
+ * @return int
+ */
 int compute_frequency_error(struct algorithm_input *inputs, int length, struct linear_func_param *func)
 {
 	if (!inputs || !func)
