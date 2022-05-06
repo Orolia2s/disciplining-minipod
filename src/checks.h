9ce41aff600a2fe9ac34af61a86a77ed2f1b19ae
Charles Parent <charles.parent@orolia2s.com>
2022-05-06 10:55:54 +0200

    Add comments for check functions

--- src/checks.h
+++ src/checks.h
@@ -1,3 +1,30 @@
+/*
+ * liboscillator-disciplining: Disciplining Algorithm for Orolia's mRO50.
+ * Copyright (C) 2021  Spectracom SAS
+
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * @file checks.h
+ * @brief Functions used to perform check over input data of the algorithm
+ * @date 2022-05-06
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #ifndef MINIPOD_CHECKS_H
 #define MINIPOD_CHECKS_H
 
@@ -5,6 +32,10 @@
 
 #include "algorithm_structs.h"
 
+/**
+ * @enum gnss_state
+ * @brief GNSS State over all inputs of the algorithm
+ */
 enum gnss_state {
     GNSS_KO,
     GNSS_UNSTABLE,
