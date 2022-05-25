a4927a16cb8b06c840d5955c13283be5ab6fac2c
Charles Parent <charles.parent@orolia2s.com>
2022-05-25 15:25:55 +0200

    Increase fine_temperature_coeff to 5

--- src/oscillator-disciplining.c
+++ src/oscillator-disciplining.c
@@ -1486,7 +1486,7 @@ int od_process(struct od *od, const struct od_input *input,
 				if (state->status != HOLDOVER) {
 					set_state(state, HOLDOVER);
 				} else {
-					float fine_temperature_coeff = 3.0;
+					float fine_temperature_coeff = 5.0;
 					float delta_fine_temperature = 0.0;
 					float dc = 0.5;
 					float delta_temp_composite = (state->mRO_EP_temperature - state->holdover_mRO_EP_temperature) + dc*(input->temperature - state->mRO_EP_temperature);
